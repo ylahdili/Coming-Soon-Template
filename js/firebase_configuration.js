dd8f5f3e189061dad60e0d23bf09c86f0a19bbb3
Youness Lahdili <y.lahdili@gmail.com>
2024-01-26 10:26:24 +0300

    changed color of button

--- js/firebase_configuration.js
+++ js/firebase_configuration.js
@@ -37,7 +37,7 @@ $(document).ready(function () {
                     })
                         .then(function (docRef) {
                             console.log("Document written with ID: ", docRef.id);
-                            $("#notifs-form-btn").text("Notified!");
+                            $("#notifs-form-btn").text("Notified!").addClass("notified");
                         })
                         .catch(function (error) {
                             console.error("Error adding document: ", error);
