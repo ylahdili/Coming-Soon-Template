f18b34f9d512e2b3adb551eddc278974e771e65a
Youness Lahdili <y.lahdili@gmail.com>
2024-01-26 10:02:29 +0300

    removed get.time line

--- js/countdown.js
+++ js/countdown.js
@@ -2,7 +2,7 @@ var countDownDate = new Date();
     // Replace with desired date 'January 17, 2019 03:24:00', example
     countDownDate.setDate('January 31, 2024 03:24:00').getTime();
     //countDownDate.setDate(countDownDate.getDate() + 30);
-    countDownDate.getTime();
+    //countDownDate.getTime();
 
     // Update the count down every 1 second
     var x = setInterval(function () {
